bef21ca9c680ad0e57b371422843c8cbdaa4aa64
Bonnie Kwong <kwongb@eos-class.engr.oregonstate.edu>
2017-02-19 00:57:18 -0800

    working on cd changing directory

--- smallsh1.c
+++ smallsh1.c
@@ -4,27 +4,33 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <sys/timeb.h>
+
 
 // declare constants
 // Spec: support command lines with a max length of 2048 characters
 #define MAX_LENGTH 2048
 
-
+// variable to store current process ID. This pid will be used to also kill child processes
+//  before exiting
+int pidNum;
 
 // prototypes
 void prompt();
+void killProcesses();
 
 void main(){
 
- printf("hello, more comments to test vim\n");
-
- /*
- bool f = false;
-  if(f == false){
-   printf("booleans work\n");
-  }
-  */
+  pidNum = getpid();
+  printf("THe pid number is %d\n", pidNum);
   prompt();
+  int i;
+  /*for(i = 0; i < 3; i++){
+   pid_t pid = fork();
+   printf("THe child process is:%d\n", pid);
+  } */
 }
 
 void prompt(){
@@ -40,7 +46,6 @@ void prompt(){
   char enteredCommand[MAX_LENGTH +1]; 
   fgets(enteredCommand, MAX_LENGTH, stdin);
   printf("You entered: %s\n", enteredCommand); 
-  printf("The len is %lu\n", strlen(enteredCommand));
 
   //  if user types in 'exit', break out of while loop
   // if user tries to exit &
@@ -51,6 +56,25 @@ void prompt(){
    printf("exiting loop\n");
    break;
   }
+
+  if(strcmp(enteredCommand, "cd") == 0){
+   char cwd[1024];
+   getcwd(cwd, sizeof(cwd));
+   printf("Current dir:%s\n", cwd);
+   // chdir
+   chdir("cd");
+   getcwd(cwd, sizeof(cwd));
+   printf("current dir:%s\n", cwd);
+  }
  }  // end of while loop
 
 } // end of prompt() 
+
+
+
+/*
+ * This method is called by prompt() when user types in exit
+ * Purpose of this method: to kill all child jobs and processes
+ */
+void killProcesses(){
+}
